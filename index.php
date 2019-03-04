f4f5934a4df679666a001132516fd5cce2f6deeb
James <598437013@qq.com>
2019-03-04 13:49:21 +1300

    azure test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Azure test</title>
+</head>
+<body>
+    <?php
+        echo "this is for auzre deployment test";
+</body>
+</html>
